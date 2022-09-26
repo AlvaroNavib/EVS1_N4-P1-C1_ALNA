3c22940814adb7c42a3f5e42dbf7ac85539047ba
alvaroNavib <alvaronavib@gmail.com>
2022-09-26 14:56:21 -0300

    Primer commit (App1)

--- alna_proyectoNro1/urls.py
+++ alna_proyectoNro1/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app1 import views as aplicacion1
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('coso/',aplicacion1.displayCoso),
+    path('form/',aplicacion1.displayForm),
 ]
---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.https import HttpResponse
+
+# Create your views here.
+def displayCoso(request):
+    return HttpResponse("<h1>Titulo</h1><nav><ul><li>elemento1</li></ul></nav><h4>coso</h4>")
+
+def displayForm(request):
+    return HttpResponse("<h2>Bienvenido</h2><div><form><input type='text' placeholder='Ingrese nombre'></input><a href=''>link</a></div></form>")
